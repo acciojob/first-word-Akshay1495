3480a5ea2fae3ddf3d138d0dda2c19c08f1d4ff6
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-21 20:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
-  // your code here
+  if (s === "") {
+    return "";
+  }
+  const spaceIndex = s.indexOf(' ');
+  if (spaceIndex === -1) {
+    return s;
+  }
+  return s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
