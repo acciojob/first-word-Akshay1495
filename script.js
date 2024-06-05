e4e81d94fb4bc7d693105fd76cd738851e53df39
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-05 19:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function firstWord(str) {
     let str1 = str.split(" ");    
     return '"' + str1[0] + '"';     
 } 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+
 
 // Do not change the code below
 
