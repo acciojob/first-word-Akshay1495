5a02a1a44e9811bf22f17ad94c4e4c9f38d86f79
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-05 19:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function firstWord(s) {
-  s = s.trimStart();
-  if (s === "") {
-    return "";
-  }
-  const spaceIndex = s.indexOf(' ');
-  if (spaceIndex === -1) {
-    return s;
-  }
-  return s.slice(0, spaceIndex);
-}
+function firstWord(str) {
 
+    let str1 = str.split(" ");    
+    return '"' + str1[0] + '"';     
+} 
+const s = prompt("Enter String:");
+alert(firstWord(s));
 
 // Do not change the code below
 
